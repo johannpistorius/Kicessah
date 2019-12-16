190046ec570c4d90e85d1c5cba6e981b37670783
Celia Ladret <ladretc@ens-epua.univ-savoie.fr>
2019-12-16 17:35:28 +0100

    finito

--- Network/API.php
+++ Network/API.php
@@ -3,6 +3,16 @@ ini_set("display_errors",1);
 $nomFichier="picture_to_process.jpg";
 $PathDuProgPython = "test.py";
 
+if (isset ($_POST['text'])){
+	echo $_POST['text'];
+	$content = file_get_contents("log.txt");
+	$date = new DateTime();
+	$date = $date->format("d/m/y h:i:s");
+	file_put_contents("log.txt",$content."\n".$date." : ".$_POST['text']);
+	die();
+}
+/**
+
 if(isset($_FILES['fichier'])){
 	$file = $_FILES['fichier']['tmp_name'];
 	if(!move_uploaded_file($file,$nomFichier)){
@@ -18,4 +28,5 @@ else{
 	echo "\n fichier non défini";
 	die();
 }
+**/
 ?>
