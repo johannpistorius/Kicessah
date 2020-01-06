52b4cb8d19971d6ca1a54f3df293823137377bbf
johannpistorius <johann.pistorius0@gmail.com>
2020-01-06 11:06:38 +0100

    face detection : inception v3

--- serveur/face_detection/face_recognition.py
+++ serveur/face_detection/face_recognition.py
@@ -5,3 +5,25 @@ Created on Mon Nov 25 08:27:44 2019
 @author: pistorij
 """
 
+import numpy as np
+import cv2
+
+def face_conv(img,face_location):
+    """
+    Function that recognizes faces from database
+    
+    Parameters
+    ----------
+    img : Image
+        picture to process
+    face_location : str
+        location of face on image
+        
+    Returns
+    -------
+    name
+        name of detected person
+    """
+    for(x,y,w,h) in face_location:
+        #TODO    
+    return name
--- serveur/face_detection/main.py
+++ serveur/face_detection/main.py
@@ -4,11 +4,16 @@ Created on Mon Nov 25 08:24:41 2019
 
 @author: pistorij
 """
-
+import cv2
 import face_detection as fd
 import face_recognition as fr
 
+img = cv2.imread('../picture_to_process.jpg')
+
 #face detection
-face_location = fd.face_roi('picture_to_process')
+face_location = fd.face_roi(img)
 
 #face recognition
+name = fr.face_conv(img,face_location)
+
+print(name)
